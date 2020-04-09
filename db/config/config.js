74d4d4ad34f9d8a921c6351ab65f56d83e968ab5
vincentayorinde <vincentayorinde@yahoo.com>
2020-04-09 05:04:04 +0100

    install body-parser consola cors dotenv express morgan mysql mysql2 nodemon path sequelize setup server script setup database connection

---
+++ db/config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'mysql',
+        logging: false,
+    },
+    test: {
+        use_env_variable: 'TEST_DATABASE_URL',
+        dialect: 'mysql',
+        logging: false,
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'mysql',
+        logging: false,
+    },
+};
