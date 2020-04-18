dbafa5049e608d1caf7ed87c8a373ef587d98ffb
vincentayorinde <vincentayorinde@yahoo.com>
2020-04-18 22:26:38 +0100

    add controller add route

--- routes/v1/vote.js
+++ routes/v1/vote.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import Validation from '../../validators/vote';
+import Middleware from '../../middleware';
 import Vote from '../../controllers/vote';
 
 const router = express.Router();
 
 router.post('/', Validation.addVote, Vote.addVote);
+router.get(
+    '/result',
+    Middleware.authenticate,
+    Middleware.isAdmin,
+    Vote.getResult
+);
 
 export default router;
