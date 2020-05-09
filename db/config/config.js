c4347fe5586f38ca9068bf205e72369eead18149
vincentayorinde <vincentayorinde@yahoo.com>
2020-05-09 15:38:07 +0100

    setup postgres

--- db/config/config.js
+++ db/config/config.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 module.exports = {
     development: {
         use_env_variable: 'DATABASE_URL',
-        dialect: 'mysql',
+        dialect: 'postgres',
         logging: false,
     },
     test: {
         use_env_variable: 'TEST_DATABASE_URL',
-        dialect: 'mysql',
+        dialect: 'postgres',
         logging: false,
     },
     production: {
         use_env_variable: 'DATABASE_URL',
-        dialect: 'mysql',
+        dialect: 'postgres',
         logging: false,
     },
 };
